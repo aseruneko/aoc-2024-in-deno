156dd0416fc1316a4212cdceb29d62659368e23b
aseruneko::kaz <aseruneko@gmail.com>
2024-12-03 08:45:42 +0900

    till day02

---
+++ day_01/similarityScore.ts
@@ -0,0 +1,16 @@
+Deno.readTextFile('./inputs/day_01_input').then((txt: string) => {
+    const rows = txt.split('\r\n')
+    const left: number[] = [];
+    const freq: Map<number, number> = new Map();
+    rows.forEach((row) => {
+        const [ls, rs] = row.split('   ');
+        const [l, r] = [parseInt(ls), parseInt(rs)]
+        left.push(l);
+        freq.set(r, (freq.get(r) ?? 0) + 1);
+    })
+    let score = 0;
+    left.forEach((l) => {
+        score += l * (freq.get(l) ?? 0)
+    })
+    console.log(score);
+})
