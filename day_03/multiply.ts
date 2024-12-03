ea424c58136e2752e004c60063e7fb3559bbe5a9
aseruneko::kaz <aseruneko@gmail.com>
2024-12-04 04:34:55 +0900

    day03

---
+++ day_03/multiply.ts
@@ -0,0 +1,10 @@
+
+Deno.readTextFile('./inputs/day_03_input').then((txt: string) => {
+    const matched = (txt.match(/mul\([0-9]+,[0-9]+\)/g) ?? []);
+    const products = matched.map(multiStr);
+    console.log(products.reduce((acc, car) => (acc + car)));
+})
+
+function multiStr(str: string): number {
+    return (str.match(/[0-9]+/g) ?? []).map((r) => parseInt(r)).reduce((acc, car) => (acc * car), 1)
+}
---
+++ day_03/multiplyWithState.ts
@@ -0,0 +1,44 @@
+Deno.readTextFile('./inputs/day_03_input').then((txt: string) => {
+    const matched: string[] = (txt.match(instructionRegExp) ?? []);
+    const mulInstructions = extract(matched.map(strToInstruction));
+    console.log(mulInstructions.reduce((acc, car) => (acc + car.a * car.b), 0));
+})
+
+function multiple(str: string): number {
+    return (str.match(/[0-9]+/g) ?? []).map((r) => parseInt(r)).reduce((acc, car) => (acc * car), 1)
+}
+
+const instructionRegExp = /(mul\([0-9]+,[0-9]+\))|(do\(\))|(don't\(\))/g
+
+type Instruction = { type: 'DO' } | { type: 'DONT' } | MulInstruction
+type MulInstruction = { type: 'MUL', a: number, b: number }
+
+function strToInstruction(str: string): Instruction | undefined {
+    if (str === 'do()') return { type: 'DO' }
+    if (str === "don't()") return { type: 'DONT' }
+    const nums = str.match(/[0-9]+/g) ?? []
+    if (nums.length === 2) return { type: 'MUL', a: parseInt(nums[0]), b: parseInt(nums[1]) }
+    return undefined;
+}
+
+function extract(inputs: (Instruction | undefined)[]): MulInstruction[] {
+    let enable = true;
+    let outputs: MulInstruction[] = [];
+
+    for (const input of inputs) {
+        if(input) {
+            switch(input.type) {
+                case 'DO':
+                    enable = true;
+                    break;
+                case 'DONT':
+                    enable = false;
+                    break;
+                case 'MUL':
+                    if(enable) outputs.push(input);
+                    break;
+            }
+        }
+    }
+    return outputs;
+}
